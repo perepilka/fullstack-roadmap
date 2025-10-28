c2d7b99116afcb283aded54678ef8cb57489a1fd
perepilka <yaroslavperepilka@gmail.com>
2025-10-28 21:48:22 +0100

    feat(todo): add TodoDto

---
+++ phase-1-core-skills/todo-app/todo-backend/src/main/java/com/perepilka/todo_backend/todo/TodoDto.java
@@ -0,0 +1,53 @@
+package com.perepilka.todo_backend.todo;
+
+import java.time.LocalDateTime;
+
+public class TodoDto {
+
+    private Long id;
+    private String title;
+    private boolean completed = false;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
