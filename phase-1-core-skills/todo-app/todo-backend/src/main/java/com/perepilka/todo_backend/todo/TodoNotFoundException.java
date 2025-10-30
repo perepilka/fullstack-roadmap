e67c299e819ff578a7fb054631ca77374b158f3c
perepilka <yaroslavperepilka@gmail.com>
2025-10-30 10:53:04 +0100

    refactor(exception): remove ResponseStatus annotation from TodoNotFoundException

--- phase-1-core-skills/todo-app/todo-backend/src/main/java/com/perepilka/todo_backend/todo/TodoNotFoundException.java
+++ phase-1-core-skills/todo-app/todo-backend/src/main/java/com/perepilka/todo_backend/todo/TodoNotFoundException.java
@@ -3,7 +3,6 @@ package com.perepilka.todo_backend.todo;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TodoNotFoundException extends RuntimeException {
     public TodoNotFoundException(Long id) {
         super("Could not find todo with id: " + id);
