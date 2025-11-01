8f8297b3ae5cea605139db8007fd559dbf0f2675
perepilka <yaroslavperepilka@gmail.com>
2025-11-01 21:59:10 +0100

    fix(auth): correct typo in UsernameAlreadyExistsException message

--- phase-1-core-skills/todo-app/todo-backend/src/main/java/com/perepilka/todo_backend/auth/UsernameAlreadyExistsException.java
+++ phase-1-core-skills/todo-app/todo-backend/src/main/java/com/perepilka/todo_backend/auth/UsernameAlreadyExistsException.java
@@ -2,6 +2,6 @@ package com.perepilka.todo_backend.auth;
 
 public class UsernameAlreadyExistsException extends RuntimeException {
     public UsernameAlreadyExistsException(String username) {
-        super("User with this username alredy exists: " + username);
+        super("User with this username already exists: " + username);
     }
 }
