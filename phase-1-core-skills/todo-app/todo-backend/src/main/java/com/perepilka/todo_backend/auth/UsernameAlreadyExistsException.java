c9dc90d4e4d08d3db8d663e87dce6e722dd9413f
perepilka <yaroslavperepilka@gmail.com>
2025-11-01 21:08:51 +0100

    feat(auth): add UsernameAlreadyExistsException for user registration error handling

---
+++ phase-1-core-skills/todo-app/todo-backend/src/main/java/com/perepilka/todo_backend/auth/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.perepilka.todo_backend.auth;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String username) {
+        super("User with this username alredy exists: " + username);
+    }
+}
