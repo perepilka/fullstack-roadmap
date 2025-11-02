427d9891c21d099e769710192700cc31fc5a83b7
perepilka <yaroslavperepilka@gmail.com>
2025-11-02 15:22:32 +0100

    feat(auth): add JpaUserDetailsService for loading user details by username

---
+++ phase-1-core-skills/todo-app/todo-backend/src/main/java/com/perepilka/todo_backend/user/JpaUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.perepilka.todo_backend.user;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JpaUserDetailsService implements UserDetailsService {
+
+    private AppUserRepository appUserRepository;
+
+    public JpaUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return appUserRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
